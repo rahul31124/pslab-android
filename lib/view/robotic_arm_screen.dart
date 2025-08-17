d16db2d56451feb287e0c32bf3d35f14f4c1ccde
Rahul Das <rdass87871@gmail.com>
2025-08-17 19:54:15 +0530

    Update robotic_arm_screen.dart

--- lib/view/robotic_arm_screen.dart
+++ lib/view/robotic_arm_screen.dart
@@ -8,6 +8,8 @@ import 'package:pslab/view/widgets/robotic_arm_dialog.dart';
 import 'package:pslab/view/widgets/robotic_arm_summary.dart';
 import 'package:pslab/view/widgets/robotic_arm_timeline.dart';
 import '../providers/robotic_arm_state_provider.dart';
+import 'logged_data_screen.dart';
+import 'package:pslab/view/widgets/guide_widget.dart';
 import 'widgets/servo_card.dart';
 
 class RoboticArmScreen extends StatefulWidget {
@@ -20,6 +22,8 @@ class RoboticArmScreen extends StatefulWidget {
 class _RoboticArmScreenState extends State<RoboticArmScreen> {
   late RoboticArmStateProvider provider;
   late List<String> servoLabels;
+  bool _showGuide = false;
+  static const imagePath = 'assets/images/robotic_arm_guide.png';
   AppLocalizations appLocalizations = getIt.get<AppLocalizations>();
   @override
   void initState() {
@@ -44,6 +48,32 @@ class _RoboticArmScreenState extends State<RoboticArmScreen> {
         ),
       );
     };
+  }
+
+  @override
+  void dispose() {
+    provider.disposeResources();
+    super.dispose();
+  }
+
+  void _hideInstrumentGuide() {
+    setState(() {
+      _showGuide = false;
+    });
+  }
+
+  List<Widget> _getRoboticArmContent() {
+    return [
+      InstrumentIntroText(
+        text: appLocalizations.roboticArmIntro,
+      ),
+      const InstrumentImage(
+        imagePath: imagePath,
+      ),
+      InstrumentIntroText(
+        text: appLocalizations.roboticArmConnection,
+      ),
+    ];
   }
 
   void _showAngleInputDialog(BuildContext context, int index) {
@@ -160,18 +190,147 @@ class _RoboticArmScreenState extends State<RoboticArmScreen> {
               IconButton(
                 icon: const Icon(Icons.save, color: Colors.white),
                 tooltip: appLocalizations.saveData,
-                onPressed: () {}, //TODO
+                onPressed: () async {
+                  final TextEditingController fileNameController =
+                      TextEditingController();
+
+                  final String? enteredFileName = await showDialog<String>(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (context) {
+                      return Dialog(
+                        insetPadding: EdgeInsets.zero,
+                        backgroundColor: Colors.transparent,
+                        child: Align(
+                          alignment: Alignment.topCenter,
+                          child: Container(
+                            margin: EdgeInsets.zero,
+                            padding: const EdgeInsets.all(8),
+                            width: 220,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  appLocalizations.enterFileName,
+                                  style: TextStyle(fontSize: 9),
+                                ),
+                                const SizedBox(height: 2),
+                                TextField(
+                                  controller: fileNameController,
+                                  style: const TextStyle(fontSize: 12),
+                                  decoration: const InputDecoration(
+                                    isDense: true,
+                                    contentPadding: EdgeInsets.symmetric(
+                                        horizontal: 6, vertical: 6),
+                                    border: OutlineInputBorder(),
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    SizedBox(
+                                      height: 26,
+                                      child: TextButton(
+                                        style: TextButton.styleFrom(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 6),
+                                          minimumSize: Size.zero,
+                                          tapTargetSize:
+                                              MaterialTapTargetSize.shrinkWrap,
+                                        ),
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(null),
+                                        child: Text(appLocalizations.cancel,
+                                            style: TextStyle(
+                                                fontSize: 10,
+                                                color: Colors.black)),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 6),
+                                    SizedBox(
+                                      height: 26,
+                                      child: TextButton(
+                                        style: TextButton.styleFrom(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 6),
+                                          minimumSize: Size.zero,
+                                          tapTargetSize:
+                                              MaterialTapTargetSize.shrinkWrap,
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context).pop(
+                                              fileNameController.text.trim());
+                                        },
+                                        child: Text(appLocalizations.save,
+                                            style: TextStyle(
+                                                fontSize: 10,
+                                                color: Colors.black)),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+
+                  try {
+                    await provider.exportTimelineToCsv(
+                      instrumentName: appLocalizations.roboticArmTitle,
+                      fileName: (enteredFileName!),
+                    );
+
+                    if (!context.mounted) return;
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(appLocalizations.fileSaved)),
+                    );
+                  } catch (e) {
+                    if (!context.mounted) return;
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(appLocalizations.csvSavingError)),
+                    );
+                  }
+                },
               ),
               IconButton(
                 icon: const Icon(Icons.info, color: Colors.white),
                 tooltip: appLocalizations.showGuide,
-                onPressed: () {}, //TODO
+                onPressed: () {
+                  setState(() {
+                    _showGuide = !_showGuide;
+                  });
+                },
               ),
               PopupMenuButton<String>(
                 icon: const Icon(Icons.more_vert, color: Colors.white),
-                onSelected: (value) {
+                onSelected: (value) async {
                   if (value == appLocalizations.showLoggedData) {
-                    // TODO
+                    final List<List<dynamic>>? data = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => LoggedDataScreen(
+                          instrumentName: appLocalizations.roboticArmTitle,
+                          appBarName: appLocalizations.showLoggedData,
+                          instrumentIcon: 'assets/icons/robotic_arm.png',
+                        ),
+                      ),
+                    );
+
+                    if (data != null) {
+                      setState(() {
+                        provider.importTimelineFromCsv(data);
+                      });
+                    }
                   }
                 },
                 itemBuilder: (BuildContext context) => [
@@ -182,67 +341,78 @@ class _RoboticArmScreenState extends State<RoboticArmScreen> {
                 ],
               ),
             ],
-            body: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: servoHeight,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: List.generate(4, (index) {
-                          return Expanded(
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 1),
-                              child: SizedBox(
-                                height: servoHeight,
-                                child: ServoCard(
-                                  value: provider.servoValues[index],
-                                  label: servoLabels[index],
-                                  servoId: index,
-                                  onChanged: (val) {
-                                    setState(() {
-                                      provider.updateServoValue(index, val);
-                                    });
-                                  },
-                                  onTap: () =>
-                                      _showAngleInputDialog(context, index),
-                                  cardHeight: servoHeight,
+            body: Stack(
+              children: [
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: servoHeight,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: List.generate(4, (index) {
+                              return Expanded(
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 1),
+                                  child: SizedBox(
+                                    height: servoHeight,
+                                    child: ServoCard(
+                                      value: provider.servoValues[index],
+                                      label: servoLabels[index],
+                                      servoId: index,
+                                      onChanged: (val) {
+                                        setState(() {
+                                          provider.updateServoValue(index, val);
+                                        });
+                                      },
+                                      onTap: () =>
+                                          _showAngleInputDialog(context, index),
+                                      cardHeight: servoHeight,
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            ),
-                          );
-                        }),
-                      ),
-                    ),
-                    const SizedBox(height: 2),
-                    Expanded(
-                      child: Scrollbar(
-                        controller: provider.timelineScrollController,
-                        thumbVisibility: true,
-                        thickness: screenHeight * 0.006,
-                        radius: const Radius.circular(4),
-                        child: TimelineScrollView(
-                          totalTimelineItems: provider.totalTimelineItems,
-                          screenHeight: screenHeight,
-                          timelinePosition: provider.timelinePosition,
-                          timelineDegrees: provider.timelineDegrees,
-                          scrollController: provider.timelineScrollController,
-                          onUpdate: (index, servo, value) {
-                            setState(() {
-                              provider.updateTimelineDegree(
-                                  index, servo, value);
-                            });
-                          },
+                              );
+                            }),
+                          ),
                         ),
-                      ),
-                    )
-                  ],
+                        const SizedBox(height: 2),
+                        Expanded(
+                          child: Scrollbar(
+                            controller: provider.timelineScrollController,
+                            thumbVisibility: true,
+                            thickness: screenHeight * 0.006,
+                            radius: const Radius.circular(4),
+                            child: TimelineScrollView(
+                              totalTimelineItems: provider.totalTimelineItems,
+                              screenHeight: screenHeight,
+                              timelinePosition: provider.timelinePosition,
+                              timelineDegrees: provider.timelineDegrees,
+                              scrollController:
+                                  provider.timelineScrollController,
+                              onUpdate: (index, servo, value) {
+                                setState(() {
+                                  provider.updateTimelineDegree(
+                                      index, servo, value);
+                                });
+                              },
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+                if (_showGuide)
+                  InstrumentOverviewDrawer(
+                    instrumentName: appLocalizations.roboticArmTitle,
+                    content: _getRoboticArmContent(),
+                    onHide: _hideInstrumentGuide,
+                  ),
+              ],
             ),
           );
         },
